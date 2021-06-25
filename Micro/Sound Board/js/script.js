4ae61c47119153d04ebdfd6d5afb19ec2588bf2c
aravinthraaj <aravinthraajk@gmail.com>
2021-06-25 22:25:14 +0200

    Sound Board

--- Micro/Sound Board/js/script.js
+++ Micro/Sound Board/js/script.js
@@ -1,1 +1,34 @@
-IndexFile;
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+let previousSong;
+
+sounds.forEach((sound) => {
+	const btn = document.createElement('button');
+	btn.classList.add('btn');
+
+	btn.innerText = sound;
+
+	btn.addEventListener('click', () => {
+		// stopSongs();
+		try {
+		
+			document.getElementById(previousSong).pause();
+			console.log('i am at try');
+		} catch (err) {
+
+		} 
+
+		document.getElementById(sound).play();
+		previousSong = sound;
+	});
+
+	document.body.appendChild(btn);
+});
+
+// function stopSongs() {
+//     sounds.forEach((sound) => {
+//         const song = document.getElementById(sound);
+
+//         song.pause();
+//         song.currentTime = 0;
+//     });
+// }
