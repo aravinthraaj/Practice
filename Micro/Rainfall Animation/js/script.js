fd2564805a7c5092820de669ff850917e1dfbcc9
aravinthraaj <aravinthraajk@gmail.com>
2021-06-25 12:14:20 +0200

    Rainfall Animation

--- Micro/Rainfall Animation/js/script.js
+++ Micro/Rainfall Animation/js/script.js
@@ -5,6 +5,9 @@ function rainFall() {
 	heart.style.right = Math.random() * 100 + 'vw';
 	heart.style.animationDuration = Math.random() * 2 + 3 + 's';
 	document.body.appendChild(heart);
+	setTimeout(() => {
+		heart.remove();
+	}, 4000);
 }
 
 setInterval(rainFall, 300);
