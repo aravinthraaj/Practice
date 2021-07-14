c6ce0d71a4a1b6ab68725f241836b0772e5c9c0b
aravinthraaj <aravinthraajk@gmail.com>
2021-07-14 23:27:34 +0200

    Recepie app

--- mini/Recipe App/js/script.js
+++ mini/Recipe App/js/script.js
@@ -1,1 +1,30 @@
-IndexFile;
+const searchBtn = document.getElementById('search');
+const panel = document.getElementById('panel');
+searchBtn.addEventListener('click', () => {
+	const searchText = document.getElementById('input-text');
+	console.log(searchText.value);
+});
+
+async function getRandomMeal() {
+	const resp = await fetch(
+		'https://www.themealdb.com/api/json/v1/1/random.php'
+	);
+	const respData = await resp.json();
+	const randomMealData = respData.meals[0];
+	console.log(respData.meals[0]);
+	addMeal(randomMealData, true);
+}
+
+getRandomMeal();
+
+function addMeal(randomMealData, random = false) {
+	const randomMeal = document.createElement('div');
+	randomMeal.classList.add('random--meal');
+	panel.appendChild(randomMeal);
+	randomMeal.innerHTML = `
+    <div class="random--meal__image">
+    ${random ? `<span> ${randomMealData.strMeal}</span>` : ''}
+    <img src="${randomMealData.strMealThumb}" alt="${randomMealData.strMeal}">
+    </div>  
+    `;
+}
